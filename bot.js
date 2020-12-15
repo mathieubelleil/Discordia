0031603545e8ade8cbb052df6795afc283086ba7
Math <mathieu.belleil@orange.fr>
2020-12-15 15:45:41 +0100

    test

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ var connection = mysql.createConnection({
 bot.login("N/jQwODQ1NjQ3Mzg1NDYwNzM3.Xb_wBA.GcnKTNo_H9yAdwvA6ic8o4_ANhM");
 
 bot.on('ready', function () {
-  console.log("Coucou ! Je suis connecté !");
+  console.log("Coucou ! Je suis présent !");
   Classes.parse(bot);
   Races.parse(bot);
 })
