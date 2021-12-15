af501be1584b666af424061843f9ea5a7b99b6b5
Adam Dupuis <adamdupuis@laposte.net>
2021-12-15 10:02:58 +0100

    Remove unnecessary db connections

--- command/Class.js
+++ command/Class.js
@@ -1,14 +1,6 @@
 const Discord = require('discord.js');
 const Command = require('./command');
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'mysql-jimg.alwaysdata.net',
-  user     : 'jimg_jdr',
-  password : 'Jdr123456,',
-  database : 'jimg_jdr'
-});
-
 module.exports = class Class extends Command {
     static match (client){
         const channel = client.channels.cache.get('656118120540536853');
@@ -21,12 +13,12 @@ module.exports = class Class extends Command {
             for(var i = 0; i < results.length;i++){
                 if(results[i].enable == 1){
                     var emoji_homme = client.emojis.cache.find(emoji => emoji.name == results[i].react);
-                    
+
                     const embed = new Discord.MessageEmbed()
                         .setColor(results[i].color)
                         .setDescription(`${emoji_homme}`+' **'+results[i].nom+"** ("+results[i].pv+"PV - "+results[i].dg+"DG) - "+results[i].description+"\n\n"+results[i].pouvoir+"\n\n\n");
                     channel.send({embed});
-                    
+
                 }
             };
             channel.send("Choix de la classe ?").then(async msg => {
