6ed7dd4247efb109969bf66f3559ddb9324399b7
Adam Dupuis <adamdupuis@laposte.net>
2022-01-25 13:58:53 +0100

    DB connection made with config.json + deleted env files

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
+const { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } = require('./config.json');
 const mysql = require('mysql');
+
+
 const connection = mysql.createConnection({
-  host     : process.env.DB_HOST,
-  user     : process.env.DB_USER,
-  password : process.env.DB_PASSWORD,
-  database : process.env.DB_NAME
+  host     : DB_HOST,
+  user     : DB_USER,
+  password : DB_PASSWORD,
+  database : DB_NAME
 });
 
 module.exports = {
