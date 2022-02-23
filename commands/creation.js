1ec6d3ece16314323df029f2446efe27c207c884
Adam Dupuis <39861076+DivermanCiv@users.noreply.github.com>
2022-02-23 12:01:23 +0100

    Features/addplayerstobdd
    * creation change
    
    * addPlayersToBDD
    
    Co-authored-by: NCH97 <Blingyb.chnn@gmaul.com>

--- commands/creation.js
+++ commands/creation.js
@@ -10,12 +10,20 @@ module.exports = {
     .setDescription('create a character'),
   async execute(client, interaction) {
     const user = interaction.user.id
-    const channel = await client.channels.cache.get(creation_perso);
     const guild = await client.guilds.cache.get(guildId);
+    const channel = await client.channels.cache.get(creation_perso);
+    let races;
+    let classes;
+    let gender;
+    let pv = 0;
+    let dg = 0;
+    let identifiers = [];
+    
     connection.query('SELECT * FROM races ORDER BY nom', function (error, results, fields) {
       if (error) throw error;
           let embeds = new Array();
           let buttons = new Array();
+          races = results;
           for(let i = 0; i < results.length;i++){
               if(results[i].enable == 1){
                   let emoji_homme = client.emojis.cache.find(emoji => emoji.name == results[i].react+"_Homme");
@@ -42,6 +50,7 @@ module.exports = {
           const row = new MessageActionRow().addComponents(buttons);
           interaction.reply({ content: 'Bienvenue dans la création du personnage, dans un premier temps, fais un choix de race parmi cette liste :', ephemeral: true, embeds: embeds, components: [row] }).then(async msg => {
             let step = 1;
+            let member = guild.members.cache.get(user)
             await client.on('interactionCreate', interactionButton => {
               if (interactionButton.isButton()){
                 const buttonId = interactionButton.customId;
@@ -49,6 +58,7 @@ module.exports = {
                 if (step === 1) {
                   connection.query('SELECT * FROM classes ORDER BY nom', function (error2, results2, fields2) {
                     if (error2) throw error2;
+                    classes = results2;
                         let embeds2 = new Array();
                         let buttons2 = new Array();
                         for(let i = 0; i < results2.length;i++){
@@ -64,29 +74,84 @@ module.exports = {
                                 .setLabel("")
                                 .setStyle('PRIMARY')
                                 .setEmoji(emoji.id);
+                                
                                 buttons2.push(button);
                             }
                         };
+                        for(i in races){
+                          
+                          if(races[i].nom === id[0]){
+                            pv += Number(races[i].pv)
+                            dg += Number(races[i].dg)
+                            identifiers.push(races[i].id)
+                            
+                          }
+                        }
+                        
+                        
+                      
+                        for(i in id){
+                          //add roles to user
+                          var role= guild.roles.cache.find(role => role.name === id[i]);
+                          member.roles.add(role)
+                        }
+                        
+                        gender = id[1]
                         const row2 = new MessageActionRow().addComponents(buttons2);
                         interaction.editReply({ content: 'Tu as choisi '+id[0]+' '+id[1]+' ! \nMaintenant, choisis ta classe :', ephemeral: true, embeds: embeds2, components: [row2] })
-
+                        
+                    
                       });
                       step++;
                     } else if (step === 2) {
                       interaction.editReply({ content: 'Tu as choisi '+id[0]+' ! \nBravo le veau !', ephemeral: true, embeds: [], components: [] });
-
+                      
+                      let emoji = client.emojis.cache.find(emoji => emoji.name == 'check');
+                      
+                      let buttons3 = new Array();
+                      const button = new MessageButton()
+                            .setCustomId('validate')
+                            .setLabel('')
+                            .setStyle('PRIMARY')
+                            .setEmoji(emoji.id);
+                      buttons3.push(button)
+                      const row3 = new MessageActionRow().addComponents(buttons3);
+                      interaction.editReply({ content: 'Validez votre choix :', ephemeral: true, embeds: [], components: [row3] })
+                      for(i in classes){
+                          
+                        if(classes[i].nom === id[0]){
+                          pv+= Number(classes[i].pv)
+                          dg += Number(classes[i].dg)
+                          identifiers.push(classes[i].id)
+                        }
+                      }
+                
+                      
+                      
+                      for(i in id){
+                        //add roles to user
+                        let role= guild.roles.cache.find(role => role.name === id[i]);
+                        member.roles.add(role)
+                      }
+                      
+                      step++;
+                      
+                      
+                    } else if (step === 3) {
+                      //inserPlayerintodb
+                      connection.query(`INSERT INTO players(discord_id,pv,pv_max,dg,gender,classes_id,races_id)
+                                      VALUES(?,?,?,?,?,?,?)`,[user,pv,pv,dg,gender,identifiers[1],identifiers[0]]
+                                    )
+                      step++;
                     }
-                    const member = guild.members.cache.get(user)
-                    for(i in id){
-
-                      let role= guild.roles.cache.find(role => role.name === id[i]);
-                      member.roles.add(role)
-                    }
+                
                   }else{
                     console.log('error, you should not see this great error message')
                       }
                         });
                       });
+                      
                   });
+  
   }
 }
